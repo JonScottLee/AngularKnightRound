d0f689d4783bfe72b11b2b6854d2557b2cbb7788
Jon <lee.jon.scott@gmail.com>
2015-06-20 00:46:42 -0400

    Added logic to properly show buy/sell buttons in Shop view.

--- knight-round/app/scripts/controllers/shop.js
+++ knight-round/app/scripts/controllers/shop.js
@@ -5,6 +5,7 @@ angular
 	.controller('ShopCtrl', function ($scope, $route, Shop, Inventory, Player) {
 
 		$scope.items = Shop.items;
+		$scope.showBuyButton = true;
 
 		$scope.buyItem = function () {
 			Player.addItem(this.item);
@@ -12,10 +13,14 @@ angular
 
 		$scope.showPlayerItems = function () {
 			$scope.items = Player.getData().items;
+			$scope.showSellButton = true;
+			$scope.showBuyButton = false;
 		};
 
 		$scope.showShopItems = function () {
 			$scope.items = Shop.items;
+			$scope.showSellButton = false;
+			$scope.showBuyButton = true;
 		};
 
 	});
